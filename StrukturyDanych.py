2a431775f355ed8e4bbe3833ebe65e3318ffa21b
DrXther <dominik.k.pelka@gmail.com>
2026-04-09 23:42:38 +0200

    daily progress on the project, complete BTS and begining of AVL

--- StrukturyDanych.py
+++ StrukturyDanych.py
@@ -72,3 +72,152 @@ for i in range(num_of_arrays):
 #     printArray(tree_collection[i])
 
 
+# Binary tree search
+class BTS:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+# insertion int bts
+def insert(root, key):
+    if root is None:
+        return BTS(key)
+    if root.val == key:
+            return root
+    if root.val < key:
+            root.right = insert(root.right, key)
+    else:
+            root.left = insert(root.left, key)
+    return root
+
+# pirinting BTS in order
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val, end=" ")
+        inorder(root.right)
+
+# function to search a key in a BST
+def search(root, key):
+  
+    # Base Cases: root is null or key 
+    # is present at root
+    if root is None or root.val == key:
+        return root
+    
+    # Key is greater than root's key
+    if root.val < key:
+        return search(root.right, key)
+    
+    # Key is smaller than root's key
+    return search(root.left, key)
+
+# finds the smallest element of the tree
+def findMin(root):
+    if root.left is None:
+        print(root.val)
+    else:
+        print(root.val, end=" ")
+        findMin(root.left)
+
+# finds the largest element of the tree
+def findMax(root):
+    if root.right is None:
+        print(root.val)
+    else:
+        print(root.val,end=" ")
+        findMax(root.right)
+
+# helper function for node deltion
+def get_successor(curr):
+    curr = curr.right
+    while curr is not None and curr.left is not None:
+        curr = curr.left
+    return curr
+
+# node delition
+def del_node(root, x):
+    if root is None:
+        return root
+
+    # If key to be searched is in a subtree
+    if root.val > x:
+        root.left = del_node(root.left, x)
+    elif root.val < x:
+        root.right = del_node(root.right, x)
+    else:
+        # If root matches with the given key
+
+        # Cases when root has 0 children or only right child
+        if root.left is None:
+            return root.right
+
+        # When root has only left child
+        if root.right is None:
+            return root.left
+
+        # When both children are present
+        succ = get_successor(root)
+        root.val = succ.val
+        root.right = del_node(root.right, succ.val)
+        
+    return root
+
+def delete_node(root):
+    print("How many nodes to delete?")
+    n = int(input())
+    for i in range(n):
+        print("delete node with key: ")
+        key = int(input())
+        del_node(root,key)
+
+def preOrder(node):
+    if not node:
+        return
+
+    # Visit the current node first
+    # res.append(node.val)
+    print(node.val, end=" ")
+
+    # Traverse the left subtree
+    preOrder(node.left)
+
+    # Traverse the right subtree
+    preOrder(node.right)
+
+def postOrder(node, res):
+    if node is None:
+        return
+
+    # First we traverse left subtree
+    postOrder(node.left, res)
+
+    # After visiting left, traverse right subtree
+    postOrder(node.right, res)
+
+    # now we visit node
+    res.append(node.val)
+
+def drop_tree(node):
+    if node is None:
+        return
+
+    # First we traverse left subtree
+    drop_tree(node.left)
+
+    # After visiting left, traverse right subtree
+    drop_tree(node.right)
+
+    print(node.val, end=" ")
+    node.val = None
+    node.left = None
+    node.right = None
+
+
+def pre_order_search(root):
+    print("Enter the key of the sub tree you want to search: ")
+    key = int(input())
+    sub_root = search(root, key)
+    preOrder(sub_root)
+
--- dataStruckt.py
+++ dataStruckt.py
@@ -1,41 +1,184 @@
-# test python file to test code outside main file
 class Node:
-    def __init__(self, key):
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
-        self.val = key
+        self.height = 1
 
 
-# A utility function to insert
-# a new node with the given key
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    if root.val == key:
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def balance(self, node):
+        if not node:
+            return 0
+        return self.height(node.left) - self.height(node.right)
+
+    def insert(self, root, value):
+        if not root:
+            return Node(value)
+        elif value < root.value:
+            root.left = self.insert(root.left, value)
+        else:
+            root.right = self.insert(root.right, value)
+
+        root.height = 1 + max(self.height(root.left), self.height(root.right))
+        balance = self.balance(root)
+
+        # Right rotation
+        if balance > 1 and value < root.left.value:
+            return self.right_rotate(root)
+
+        # Left rotation
+        if balance < -1 and value > root.right.value:
+            return self.left_rotate(root)
+
+        # Left-Right rotation
+        if balance > 1 and value > root.left.value:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+
+        # Right-Left rotation
+        if balance < -1 and value < root.right.value:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
+        return root
+
+    def delete(self, root, value):
+        if not root:
             return root
-    if root.val < key:
-            root.right = insert(root.right, key)
-    else:
-            root.left = insert(root.left, key)
-    return root
 
+        if value < root.value:
+            root.left = self.delete(root.left, value)
+        elif value > root.value:
+            root.right = self.delete(root.right, value)
+        else:
+            if not root.left:
+                temp = root.right
+                root = None
+                return temp
+            elif not root.right:
+                temp = root.left
+                root = None
+                return temp
 
-# A utility function to do inorder tree traversal
-def inorder(root):
+            temp = self.min_value_node(root.right)
+            root.value = temp.value
+            root.right = self.delete(root.right, temp.value)
+
+        if not root:
+            return root
+
+        root.height = 1 + max(self.height(root.left), self.height(root.right))
+        balance = self.balance(root)
+
+        # Right Rotation
+        if balance > 1 and self.balance(root.left) >= 0:
+            return self.right_rotate(root)
+
+        # Left Rotation
+        if balance < -1 and self.balance(root.right) <= 0:
+            return self.left_rotate(root)
+
+        # Left-Right rotation
+        if balance > 1 and self.balance(root.left) < 0:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+
+        # Right-Left rotation
+        if balance < -1 and self.balance(root.right) > 0:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
+        return root
+
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self.height(z.left), self.height(z.right))
+        y.height = 1 + max(self.height(y.left), self.height(y.right))
+
+        return y
+
+    def right_rotate(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self.height(z.left), self.height(z.right))
+        y.height = 1 + max(self.height(y.left), self.height(y.right))
+
+        return y
+
+    def min_value_node(self, root):
+        current = root
+        while current.left:
+            current = current.left
+        return current
+    # to do:
+
+    # make max value serch when in a better state
+
+    # make pre order search
+
+    # make the droping of the tree 
+
+    # make pre order search with key
+
+    def search(self, root, value):
+        if not root or root.value == value:
+            return root
+        if root.value < value:
+            return self.search(root.right, value)
+        return self.search(root.left, value)
+
+    def insert_value(self, value):
+        self.root = self.insert(self.root, value)
+
+    def delete_value(self, value):
+        self.root = self.delete(self.root, value)
+
+    def search_value(self, value):
+        return self.search(self.root, value)
+
+tree = AVLTree()
+tree.insert_value(10)
+tree.insert_value(20)
+tree.insert_value(30)
+tree.insert_value(40)
+tree.insert_value(50)
+
+print("Tree after insertion:")
+# In-order traversal to print the tree
+def inorder_traversal(root):
     if root:
-        inorder(root.left)
-        print(root.val, end=" ")
-        inorder(root.right)
+        inorder_traversal(root.left)
+        print(root.value),
+        inorder_traversal(root.right)
 
+inorder_traversal(tree.root)
+print()
 
-r = Node(15)
-r = insert(r, 10)
-r = insert(r, 18)
-r = insert(r, 4)
-r = insert(r, 11)
-r = insert(r, 16)
-r = insert(r, 20)
-r = insert(r, 13)
+tree.delete_value(20)
+print("Tree after deletion of 20:")
+inorder_traversal(tree.root)
+print()
 
-# Print inorder traversal of the BST
-inorder(r)
+result = tree.search_value(30)
+if result:
+    print("Node found")
+else:
+    print("Node not found")
